f45531101c27da20dde91a2b0d3b00c3e3f57183
Matthias Hertel <mathertel@hotmail.com>
2019-06-05 20:40:50 +0200

    interface only from scratch

--- src/LiquidCrystal_PCF8574.cpp
+++ src/LiquidCrystal_PCF8574.cpp
@@ -1,278 +1,156 @@
-///
 /// \file LiquidCrystal_PCF8574.cpp
-/// \brief LiquidCrystal (LCD) library with PCF8574 I2C adapter.
+/// \brief LiquidCrystal library with PCF8574 I2C adapter.
 ///
-/// \copyright see: LiquidCrystal_PCF8574.h
-/// 
-/// \details
-/// This is a library for driving LiquidCrystal displays (LCD) by using the I2C bus and an PCF8574 I2C adapter.
-/// This library is derived from the original Arduino LiquidCrystal library and uses the original Wire library for communication.
-///
-/// More documentation and source code is available at http://www.mathertel.de/Arduino
+/// \author Matthias Hertel, http://www.mathertel.de
+/// \copyright Copyright (c) 2019 by Matthias Hertel.
 ///
 /// ChangeLog see: LiquidCrystal_PCF8574.h
 
 #include "LiquidCrystal_PCF8574.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <inttypes.h>
 #include "Arduino.h"
 #include <Wire.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <string.h>
 
 /// Definitions on how the PCF8574 is connected to the LCD
 
 /// These are Bit-Masks for the special signals and background light
-#define PCF_RS  0x01
-#define PCF_RW  0x02
-#define PCF_EN  0x04
+#define PCF_RS 0x01
+#define PCF_RW 0x02
+#define PCF_EN 0x04
 #define PCF_BACKLIGHT 0x08
 
 // Definitions on how the PCF8574 is connected to the LCD
 // These are Bit-Masks for the special signals and Background
-#define RSMODE_CMD  0
+#define RSMODE_CMD 0
 #define RSMODE_DATA 1
-
-
-// When the display powers up, it is configured as follows:
-//
-// 1. Display clear
-// 2. Function set: 
-//    DL = 1; 8-bit interface data 
-//    N = 0; 1-line display 
-//    F = 0; 5x8 dot character font 
-// 3. Display on/off control: 
-//    D = 0; Display off 
-//    C = 0; Cursor off 
-//    B = 0; Blinking off 
-// 4. Entry mode set: 
-//    I/D = 1; Increment by 1 
-//    S = 0; No shift 
-//
-// Note, however, that resetting the Arduino doesn't reset the LCD, so we
-// can't assume that its in that state when a sketch starts (and the
-// LiquidCrystal constructor is called).
 
 // modification:
 // don't use ports from Arduino, but use ports from Wire
 
 // a nibble is a half Byte
 
-// NEW: http://playground.arduino.cc//Code/LCDAPI
-// NEW: setBacklight
-
-
-LiquidCrystal_PCF8574::LiquidCrystal_PCF8574(uint8_t addr)
+LiquidCrystal_PCF8574::LiquidCrystal_PCF8574(int i2cAddr)
 {
-  _Addr = addr;
+  _i2cAddr = i2cAddr;
   _backlight = 0;
 } // LiquidCrystal_PCF8574
 
 
-void LiquidCrystal_PCF8574::begin(uint8_t cols, uint8_t lines, uint8_t dotsize) {
-  // cols ignored !
-  _numlines = lines;
-
-  _displayfunction = 0;
-
-  if (lines > 1) {
-    _displayfunction |= LCD_2LINE;
-  }
-
-  // for some 1 line displays you can select a 10 pixel high font
-  if ((dotsize != 0) && (lines == 1)) {
-    _displayfunction |= LCD_5x10DOTS;
-  }
-
-  // SEE PAGE 45/46 FOR INITIALIZATION SPECIFICATION!
-  // according to datasheet, we need at least 40ms after power rises above 2.7V
-  // before sending commands. Arduino can turn on way befor 4.5V so we'll wait 50
-  Wire.begin();
-
-  // initializing th display
-  _write2Wire(0x00, LOW, false);
-  delayMicroseconds(50000); 
-
-  // put the LCD into 4 bit mode according to the hitachi HD44780 datasheet figure 26, pg 47
-  _sendNibble(0x03, RSMODE_CMD);
-  delayMicroseconds(4500); 
-  _sendNibble(0x03, RSMODE_CMD);
-  delayMicroseconds(4500); 
-  _sendNibble(0x03, RSMODE_CMD);
-  delayMicroseconds(150);
-  // finally, set to 4-bit interface
-  _sendNibble(0x02, RSMODE_CMD);
-
-  // finally, set # lines, font size, etc.
-  _command(LCD_FUNCTIONSET | _displayfunction);  
-
-  // turn the display on with no cursor or blinking default
-  _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;  
-  display();
-
-  // clear it off
-  clear();
-
-  // Initialize to default text direction (for romance languages)
-  _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
-  // set the entry mode
-  _command(LCD_ENTRYMODESET | _displaymode);
+void LiquidCrystal_PCF8574::begin(int cols, int lines)
+{
+  // not implemented yet
 }
 
 /********** high level commands, for the user! */
 void LiquidCrystal_PCF8574::clear()
 {
-  _command(LCD_CLEARDISPLAY);  // clear display, set cursor position to zero
-  delayMicroseconds(2000);  // this command takes a long time!
+  // not implemented yet
 }
 
 void LiquidCrystal_PCF8574::home()
 {
-  _command(LCD_RETURNHOME);  // set cursor position to zero
-  delayMicroseconds(2000);  // this command takes a long time!
+  // not implemented yet
 }
 
 
-/// Set the cursor to a new position. 
-void LiquidCrystal_PCF8574::setCursor(uint8_t col, uint8_t row)
+/// Set the cursor to a new position.
+void LiquidCrystal_PCF8574::setCursor(int col, int row)
 {
-  int row_offsets[] = { 0x00, 0x40, 0x14, 0x54   };
-  if ( row >= _numlines ) {
-    row = _numlines-1;    // we count rows starting w/0
-  }
-
-  _command(LCD_SETDDRAMADDR | (col + row_offsets[row]));
+  // not implemented yet
 }
 
 // Turn the display on/off (quickly)
-void LiquidCrystal_PCF8574::noDisplay() {
-  _displaycontrol &= ~LCD_DISPLAYON;
-  _command(LCD_DISPLAYCONTROL | _displaycontrol);
+void LiquidCrystal_PCF8574::noDisplay()
+{
+  // not implemented yet
 }
-void LiquidCrystal_PCF8574::display() {
-  _displaycontrol |= LCD_DISPLAYON;
-  _command(LCD_DISPLAYCONTROL | _displaycontrol);
+
+void LiquidCrystal_PCF8574::display()
+{
+  // not implemented yet
 }
 
 // Turns the underline cursor on/off
-void LiquidCrystal_PCF8574::noCursor() {
-  _displaycontrol &= ~LCD_CURSORON;
-  _command(LCD_DISPLAYCONTROL | _displaycontrol);
+void LiquidCrystal_PCF8574::noCursor()
+{
+  // not implemented yet
 }
-void LiquidCrystal_PCF8574::cursor() {
-  _displaycontrol |= LCD_CURSORON;
-  _command(LCD_DISPLAYCONTROL | _displaycontrol);
+
+void LiquidCrystal_PCF8574::cursor()
+{
+  // not implemented yet
 }
 
 // Turn on and off the blinking cursor
-void LiquidCrystal_PCF8574::noBlink() {
-  _displaycontrol &= ~LCD_BLINKON;
-  _command(LCD_DISPLAYCONTROL | _displaycontrol);
+void LiquidCrystal_PCF8574::noBlink()
+{
+  // not implemented yet
 }
-void LiquidCrystal_PCF8574::blink() {
-  _displaycontrol |= LCD_BLINKON;
-  _command(LCD_DISPLAYCONTROL | _displaycontrol);
+
+void LiquidCrystal_PCF8574::blink()
+{
+  // not implemented yet
 }
 
 // These commands scroll the display without changing the RAM
-void LiquidCrystal_PCF8574::scrollDisplayLeft(void) {
-  _command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
+void LiquidCrystal_PCF8574::scrollDisplayLeft(void)
+{
+  // not implemented yet
 }
-void LiquidCrystal_PCF8574::scrollDisplayRight(void) {
-  _command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
+
+void LiquidCrystal_PCF8574::scrollDisplayRight(void)
+{
+  // not implemented yet
 }
 
 // This is for text that flows Left to Right
-void LiquidCrystal_PCF8574::leftToRight(void) {
-  _displaymode |= LCD_ENTRYLEFT;
-  _command(LCD_ENTRYMODESET | _displaymode);
+void LiquidCrystal_PCF8574::leftToRight(void)
+{
+  // not implemented yet
 }
 
 // This is for text that flows Right to Left
-void LiquidCrystal_PCF8574::rightToLeft(void) {
-  _displaymode &= ~LCD_ENTRYLEFT;
-  _command(LCD_ENTRYMODESET | _displaymode);
+void LiquidCrystal_PCF8574::rightToLeft(void)
+{
+  // not implemented yet
 }
 
 // This will 'right justify' text from the cursor
-void LiquidCrystal_PCF8574::autoscroll(void) {
-  _displaymode |= LCD_ENTRYSHIFTINCREMENT;
-  _command(LCD_ENTRYMODESET | _displaymode);
+void LiquidCrystal_PCF8574::autoscroll(void)
+{
+  // not implemented yet
 }
 
 // This will 'left justify' text from the cursor
-void LiquidCrystal_PCF8574::noAutoscroll(void) {
-  _displaymode &= ~LCD_ENTRYSHIFTINCREMENT;
-  _command(LCD_ENTRYMODESET | _displaymode);
+void LiquidCrystal_PCF8574::noAutoscroll(void)
+{
+  // not implemented yet
 }
 
 
 /// Setting the brightness of the background display light.
 /// The backlight can be switched on and off.
 /// The current brightness is stored in the private _backlight variable to have it available for further data transfers.
-void LiquidCrystal_PCF8574::setBacklight(uint8_t brightness) {
-  _backlight = brightness;
-  // send no data but set the background-pin right;
-  _write2Wire(0x00, RSMODE_DATA, false);
-} // setBacklight
+void LiquidCrystal_PCF8574::setBacklight(int brightness)
+{
+  // not implemented yet
+}
 
 
 // Allows us to fill the first 8 CGRAM locations
 // with custom characters
-void LiquidCrystal_PCF8574::createChar(uint8_t location, uint8_t charmap[]) {
-  location &= 0x7; // we only have 8 locations 0-7
-  _command(LCD_SETCGRAMADDR | (location << 3));
-  for (int i=0; i<8; i++) {
-    write(charmap[i]);
-  }
+void LiquidCrystal_PCF8574::createChar(int location, int charmap[])
+{
+  // not implemented yet
 }
 
 /* The write function is needed for derivation from the Print class. */
-inline size_t LiquidCrystal_PCF8574::write(uint8_t value) {
-  _send(value, RSMODE_DATA);
-  return 1; // assume sucess
+inline size_t LiquidCrystal_PCF8574::write(uint8_t ch)
+{
+  // not implemented yet
 }
-
-/* ----- low level functions ----- */
-
-inline void LiquidCrystal_PCF8574::_command(uint8_t value) {
-  _send(value, RSMODE_CMD);
-} // _command()
-
-
-// write either command or data
-void LiquidCrystal_PCF8574::_send(uint8_t value, uint8_t mode) {
-  // separate the 4 value-nibbles
-  uint8_t valueLo = value    & 0x0F;
-  uint8_t valueHi = value>>4 & 0x0F;
-
-  _sendNibble(valueHi, mode);
-  _sendNibble(valueLo, mode);
-} // _send()
-
-
-// write a nibble / halfByte with handshake
-void LiquidCrystal_PCF8574::_sendNibble(uint8_t halfByte, uint8_t mode) {
-  _write2Wire(halfByte, mode, true);
-  delayMicroseconds(1);    // enable pulse must be >450ns
-  _write2Wire(halfByte, mode, false);
-  delayMicroseconds(37);   // commands need > 37us to settle
-} // _sendNibble
-
-
-// private function to change the PCF8674 pins to the given value
-void LiquidCrystal_PCF8574::_write2Wire(uint8_t halfByte, uint8_t mode, uint8_t enable) {
-  // map the given values to the hardware of the I2C schema
-  uint8_t i2cData = halfByte << 4;
-  if (mode > 0) i2cData |= PCF_RS;
-  // PCF_RW is never used.
-  if (enable > 0) i2cData |= PCF_EN;
-  if (_backlight > 0) i2cData |= PCF_BACKLIGHT;
-
-  Wire.beginTransmission(_Addr);
-  Wire.write(i2cData);
-  Wire.endTransmission();   
-} // write2Wire
-
 
 // The End.
--- src/LiquidCrystal_PCF8574.h
+++ src/LiquidCrystal_PCF8574.h
@@ -1,129 +1,76 @@
-///
 /// \file LiquidCrystal_PCF8574.h
 /// \brief LiquidCrystal library with PCF8574 I2C adapter.
 ///
 /// \author Matthias Hertel, http://www.mathertel.de
-/// 
-/// \copyright Copyright (C) 2006-2008 Hans-Christoph Steiner. All rights reserved.
-/// \copyright Copyright (c) 2010 Arduino LLC. All right reserved.
-/// \copyright Copyright (c) 2014 by Matthias Hertel.\n
-/// 
-/// This library is derived from the original Arduino LiquidCrystal library
-/// and uses the original Wire library for communication.
-/// 
-/// From Arduino: 
-/// This library is free software; you can redistribute it and/or
-/// modify it under the terms of the GNU Lesser General Public
-/// License as published by the Free Software Foundation; either
-/// version 2.1 of the License, or (at your option) any later version.
-/// 
-/// From Author:
-/// The extended work is licensed under a BSD style license.\n
+///
+/// \copyright Copyright (c) 2019 by Matthias Hertel.\n
+///
+/// The library work is licensed under a BSD style license.\n
 /// See http://www.mathertel.de/License.aspx
-/// 
-/// Both License rules apply.
 ///
 /// \details
-/// This is a library for driving LiquidCrystal displays (LCD) by using the I2C bus and an PCF8574 I2C adapter.
-/// This library is derived from the original Arduino LiquidCrystal library and uses the original Wire library for communication.
+/// This library is can drive a Liquid Cristal display based on the Hitachi HD44780 chip that is connected through a PCF8574 I2C adapter.
+/// This is given by many (not all) LCD adapters. This library uses the original Wire library for communication.
+/// The API if common to many LCD libraries and documented in https://www.arduino.cc/en/Reference/LiquidCrystal.
+/// and partially functions from https://playground.arduino.cc/Code/LCDAPI/.
+
 ///
 /// ChangeLog:
 /// --------
 /// * 19.10.2013 created.
-/// * 24.05.2015 Arduino Library Manager compatible.
-
-
+/// * 05.06.2019 rewrite from scratch.
 
 #ifndef LiquidCrystal_PCF8574_h
 #define LiquidCrystal_PCF8574_h
 
-#include <inttypes.h>
 #include "Print.h"
+#include <inttypes.h>
 
-// commands
-#define LCD_CLEARDISPLAY 0x01
-#define LCD_RETURNHOME 0x02
-#define LCD_ENTRYMODESET 0x04
-#define LCD_DISPLAYCONTROL 0x08
-#define LCD_CURSORSHIFT 0x10
-#define LCD_FUNCTIONSET 0x20
-#define LCD_SETCGRAMADDR 0x40
-#define LCD_SETDDRAMADDR 0x80
-
-// flags for display entry mode
-#define LCD_ENTRYRIGHT 0x00
-#define LCD_ENTRYLEFT 0x02
-#define LCD_ENTRYSHIFTINCREMENT 0x01
-#define LCD_ENTRYSHIFTDECREMENT 0x00
-
-// flags for display on/off control
-#define LCD_DISPLAYON 0x04
-#define LCD_DISPLAYOFF 0x00
-#define LCD_CURSORON 0x02
-#define LCD_CURSOROFF 0x00
-#define LCD_BLINKON 0x01
-#define LCD_BLINKOFF 0x00
-
-// flags for display/cursor shift
-#define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE 0x00
-#define LCD_MOVERIGHT 0x04
-#define LCD_MOVELEFT 0x00
-
-// flags for function set
-#define LCD_8BITMODE 0x10
-#define LCD_4BITMODE 0x00
-#define LCD_2LINE 0x08
-#define LCD_1LINE 0x00
-#define LCD_5x10DOTS 0x04
-#define LCD_5x8DOTS 0x00
-
-class LiquidCrystal_PCF8574 : public Print {
+class LiquidCrystal_PCF8574 : public Print
+{
 public:
-  LiquidCrystal_PCF8574(uint8_t adr);
+  LiquidCrystal_PCF8574(int i2cAddr);
+  // note:
+  // There is no sda and scl parameter for i2c in any api.
+  // The Wire library has standard settings that can be overwritten by using Wire.begin(int sda, int scl) before calling LiquidCrystal_PCF8574::begin();
 
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+  // Funtions from reference:
 
-  void clear();
+  void begin(int cols, int rows);
+
+  void init();
+
   void home();
-
-  void noDisplay();
-  void display();
-  void noBlink();
-  void blink();
-  void noCursor();
+  void setCursor(int col, int row);
   void cursor();
+  void noCursor();
+  void blink();
+  void noBlink();
+  void display();
+  void noDisplay();
   void scrollDisplayLeft();
   void scrollDisplayRight();
-  void leftToRight();
-  void rightToLeft();
   void autoscroll();
   void noAutoscroll();
+  void leftToRight();
+  void rightToLeft();
+  void createChar(int, int[]);
 
-  void setBacklight(uint8_t brightness);
+  // plus functions from LCDAPI:
 
-  void createChar(uint8_t, uint8_t[]);
-  void setCursor(uint8_t col, uint8_t row); 
+  // void setDelay(int Cmd, int Char);
+  void command(int value);
+  void clear(); // same as init()
+  // setCursor() from LCDAPI
 
-  virtual size_t write(uint8_t);
-  using Print::write;
+  void setBacklight(int brightness);
+
+  virtual size_t write(uint8_t ch);
+  // using Print::write;
 
 private:
-  // low level functions
-  void _command(uint8_t);
-  void _send(uint8_t value, uint8_t mode);
-  void _sendNibble(uint8_t halfByte, uint8_t mode);
-  void _write2Wire(uint8_t halfByte, uint8_t mode, uint8_t enable);
-
-// NEW:
-  uint8_t _Addr;        ///< Wire Address of the LCD
-  uint8_t _backlight;   ///< the backlight intensity 
-
-  uint8_t _displayfunction; ///< lines and dots mode
-  uint8_t _displaycontrol;  ///< cursor, display, blink flags
-  uint8_t _displaymode;     ///< left2right, autoscroll
-
-  uint8_t _numlines;        ///< The number of rows the display supports.
+  int _i2cAddr; ///< Wire Address of the LCD
+  int _backlight; ///< the backlight intensity
 };
 
 #endif
